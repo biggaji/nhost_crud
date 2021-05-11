6eda7851826961cc099f426db7a36150230acf77
biggaji <tobiajibade2017@yandex.com>
2021-05-11 01:44:01 -0700

    started express web app

--- app.ts
+++ app.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-import * as handlebars from 'express-handlebars';
+import express, {Request, Response, NextFunction, Application} from 'express';
+// import * as handlebars from 'express-handlebars';
 
-const app = express();
-const exphbs = handlebars();
+const app:Application = express();
+// const exphbs = handlebars();
 
 // homepage
 
-app.get('/', (req,res) => {
+app.get('/', (req:Request,res:Response):void => {
     res.json({
         "message": "hello world"
     })
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT:number = 3000 || process.env.PORT;
 
 app.listen(PORT, () => {
-    `server running on ${PORT}`;
+    console.log(`server running on ${PORT}`);
 });
