59fa2f0d95786f818a65ac14496d8dec03bb7ffe
biggaji <tobiajibade2017@yandex.com>
2021-05-14 03:47:10 -0700

    fixed dotenv error

--- app.ts
+++ app.ts
@@ -1,19 +1,39 @@
-import express, {Request, Response, NextFunction, Application} from 'express';
-// import * as handlebars from 'express-handlebars';
+import * as dotenv from 'dotenv';
+import express, {Request, Response, NextFunction, Application, Router} from 'express';
+import exphbs from 'express-handlebars';
+import router  from './src/routers/index';
+import path from 'path';
+
+dotenv.config();
 
 const app:Application = express();
-// const exphbs = handlebars();
 
-// homepage
+const indexRoute:Router = router;
 
-app.get('/', (req:Request,res:Response):void => {
-    res.json({
-        "message": "hello world"
-    })
+// app.use(urlencoded({ extended: false }));
+// app.use(json());
+
+// setup static file handler
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// setup template engine
+app.engine('hbs', exphbs({defaultLayout: "main", extname: "hbs"}));
+app.set('view engine', 'hbs');
+
+
+app.use('/', indexRoute);
+
+// setup 404 page handler
+app.use((req:Request, res:Response, next:NextFunction) => {
+    res.render("404", {pageTitle: "Page Not Found"});
 });
 
-const PORT:number = 3000 || process.env.PORT;
+const ENVPORT = process.env.PORT
+const PORT: (string|number) =  ENVPORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
 });
+
+export default app;
